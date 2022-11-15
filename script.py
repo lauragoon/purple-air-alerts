4a9b6063c6c2606b2efc15df2f1ed61357c2e509
Laura Goon <goon.laura@gmail.com>
2022-11-14 22:38:21 -0800

    connected to Gmail API, and sends alerts via email

--- script.py
+++ script.py
@@ -1,8 +1,22 @@
+import base64
+from email.message import EmailMessage
+import os.path
 import requests
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+API_KEY = ""
 ROOT_URL = "https://api.purpleair.com/v1/sensors"
 LONG_LAT_BOUNDS = ()
-API_KEY = ""
+
+GMAIL_API_SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+FROM_EMAIL = ""
+TO_EMAIL = ""
 
 
 def calculate_aqi(cp, ih, il, bph, bpl):
@@ -123,9 +137,60 @@ def send_alert(impt_aqi):
     Send alert regarding sensors where AQI > 50.
 
     :param impt_aqi: dict of sensor names mapped to AQI values
-    :returns: TODO: need to link to external service
+    :returns: message object, including message ID
     """
-    print("Test send alert: " + str(impt_aqi))
+    # construct email message
+    send_msg = "There are high AQI levels detected near you: \n"
+    for k,v in impt_aqi.items():
+        send_msg += "Sensor <" + k + ">: " + str(v) + "\n"
+
+    # authenticate with Gmail API
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", GMAIL_API_SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", GMAIL_API_SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    # send email
+    try:
+        service = build("gmail", "v1", credentials=creds)
+        message = EmailMessage()
+
+        message.set_content(send_msg)
+
+        message["To"] = TO_EMAIL
+        message["From"] = FROM_EMAIL
+        message["Subject"] = "PurpleAir Alerts - High AQI Detected"
+
+        # encode message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+            .decode()
+
+        create_message = {
+            "raw": encoded_message
+        }
+        # pylint: disable=E1101
+        send_message = (service.users().messages().send
+                        (userId="me", body=create_message).execute())
+        print(F'Message Id: {send_message["id"]}')
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        send_message = None
+
+    return send_message
 
 
 def main():
